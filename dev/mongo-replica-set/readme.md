852c4c620794d0d22aef17c77cafb8bf4f02b409
atillas-git <sina.postaci.tedu@gmail.com>
2025-08-16 20:39:42 +0300

    2.0.0 is mostly finished

--- dev/mongo-replica-set/readme.md
+++
@@ -1,5 +0,0 @@
-# Running MongoDB as a replica set in your local
-In order for the batch processing to work, mongodb requires transactions.
-Transactions in mongodb require you to have a replica set.
-This document is for you to run mongodb as a replica set in your local machine.
-
--- tests/queues/postgres/postgres.batch.test.ts
+++ tests/queues/postgres/postgres.batch.test.ts
@@ -1,3 +1,11 @@
 import { PostgreSQLJobStorage,PostgreSQLJobQueue,Job,JobHandler,QueueEvent } from "../../../src";
 import { Pool } from "pg";
 import dotenv from "dotenv";
+import { beforeEach, describe } from "vitest";
+dotenv.config();
+
+describe("PostgreSQLJobQueue", () => {
+    beforeEach(() => {
+        
+    })
+})
